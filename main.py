0e3fccf98b8be935d5821eb79c64c963c2b9b850
sruthymr1 <141391248+sruthymr1@users.noreply.github.com>
2024-03-07 23:04:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,4 +10,4 @@ async def root():
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=80, reload=True)
